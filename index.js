71a00d77f3626618026022d7ef06c96ad0cb350f
MK-Saifullah <ashflower0132@gmail.com>
2022-12-31 08:26:17 +0000

    news id added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('Hello, Khalid! Your Birmingham news is on live channel');
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    if(id === '08'){
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(cat => cat.category_id === id)
+        res.send(categoryNews)
+    }
+})
+// We have filtered categories news because it contains more categories news
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    // console.log(req.params)
+    const id = req.params.id;
+    const selectedNews = news.find(news => news._id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
